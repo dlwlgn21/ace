9a7e79259024addd158334ba3cbbf533ca643ab7
이종만 <@s.gateway.2wire.net>
2017-10-21 11:05:57 +1000

    다시추가

--- src/secondlesson/Helloworld.java
+++ src/secondlesson/Helloworld.java
@@ -6,5 +6,6 @@ public class Helloworld {
     	   System.out.println("아니 근데 슈발 흠..일단 계속 배우자");
     	   System.out.println("꼭 하고 말거야 ");
            System.out.println("Github repository에 올라갔을까??");
+           System.out.println("와 올라갔다!! 대체 어떻게 가능한거지 ㅋㅋㅋㅋㅋ");
        }
 }
